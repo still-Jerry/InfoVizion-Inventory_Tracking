6c24ed36e5a6d1f4286fb7dd766b89aead6dbe46
still-Jerry <oljaapelsin@gmail.com>
2026-09-09 00:13:16 +0300

    Выпонение завершено, не хваатет проверки на дурака

--- task_01/stock.py
+++ task_01/stock.py
@@ -1,7 +1,7 @@
 print("hello")
 
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 import re 
 import duckdb
 import time
@@ -13,59 +13,63 @@ PATH_SOURCE = Path(__file__).parent
 PATH_TRANS = PATH_SOURCE / 'invent_trans'
 PATH_STOCK = PATH_SOURCE / 'stock'
 
-
 def main() -> None:
     start_time = time.time()
-     # 1. Задаем стартовую дату (2025-05-01) и конечную (2025-07-31)
-    # stock_start = duckdb.sql(f"SELECT * FROM '{PATH_STOCK}/stock_2025_04_30.csv' LIMIT 5").df()
-    # print(stock_start)
-    # invent_trans = duckdb.sql(f"SELECT * FROM '{PATH_TRANS}/invent_trans_2025_05.csv' LIMIT 5").df()
-    # print(invent_trans)
-    # use_date= datetime.now().strftime('%Y-%m-%d')
-    start_stock_files = sorted(PATH_STOCK.glob('stock_*.csv'))[-1]
+     
     invent_trans_files = sorted(PATH_TRANS.glob('invent_trans_*.csv'))
-
-    duckdb.sql(f"CREATE TEMP TABLE temp_start_stock_table AS SELECT * FROM '{start_stock_files}'")
+    
+   
     
     for file in invent_trans_files:
-        duckdb.sql(f"CREATE TEMP TABLE temp_invent_trans_table AS SELECT * FROM '{file}'")
+        duckdb.sql(f"create OR REPLACE  TEMP TABLE temp_invent_trans_table AS SELECT * FROM '{file}' ")
+        #забирает файл с новейшей датой в наименовании (последний отсортированный)
+        start_stock_files = sorted(PATH_STOCK.glob('stock_*.csv'))[-1]
+        duckdb.sql(f"CREATE OR REPLACE TEMP TABLE temp_start_stock_table AS SELECT * FROM '{start_stock_files}'")
+
+        duckdb.sql(f"CREATE OR REPLACE TEMP TABLE temp_use_stock_table AS SELECT * FROM temp_start_stock_table limit 0")
+
         # выдаёт кусок текста послностью совпадающий с шаблоном 
         file_date = re.search(r'\d{4}_\d{2}', file.name).group(0).replace('_','-')
         year=int(file_date.split('-')[0])
         month=int(file_date.split('-')[1])
         num_days=get_days_in_month(year, month)
+        flag = True #флаг для использования стартовой таблицы или забор информации из уже собранных данных
         for i in range (1,num_days):
+            #костыль использованя формата даты - забираем из файла год и месяц , дни добавляем инкриментально 
             if i <10:
                 use_date=f"{file_date}-0{i}"
             else:
                 use_date=f"{file_date}-{i}"
-            
-            output_file = PATH_STOCK / f"stock_{use_date.replace('-','_')}.csv"
-            # print(output_file)
-            result=duckdb.sql("copy (select item_id, location_id, trans_date, SUM(qty), SUM(cost_amount) from ("
-                              "select * from temp_start_stock_table union all select * from temp_invent_trans_table)"
-                              f"group by item_id, location_id, trans_date) TO '{str(output_file)}' (format csv, header true, delimiter ';');")
 
+           #переменная следующего дня , используется для расчёта остатков 
+            next_date = (datetime.strptime(use_date, "%Y-%m-%d") + timedelta(days=1)).strftime("%Y-%m-%d")
+
+            output_file = PATH_STOCK / f"stock_{use_date.replace('-','_')}.csv"
+
+            if flag:
+                use_table="temp_start_stock_table"
+                flag=False
+            else:
+                use_table="temp_use_stock_table"
             
-            # result=duckdb.sql(f"COPY (SELECT * FROM temp_invent_trans_table WHERE trans_date = '{use_date}') TO '{output_file}' (format csv, header true, delimiter ';');")
-            # result=duckdb.sql(f"SELECT * FROM temp_invent_trans_table where trans_date = '{use_date}'").df()
-            # result.to_csv(output_file, index=False, sep=';')
+            sqltext = f"""select item_id, location_id, trans_date, sum(qty) as qty,  sum(cost_amount) as cost_amount from 
+                                            (select item_id, location_id, '{next_date}' as trans_date, sum(qty) as qty , sum(cost_amount) as cost_amount from temp_invent_trans_table 
+                                            where trans_date = '{next_date}' group by 1,2, 3 UNION ALl 
+                                            select item_id, location_id, trans_date, qty, cost_amount  from {use_table} ) GROUP by 1, 2, 3 """
+            duckdb.sql("delete from temp_use_stock_table")
+            duckdb.sql(f"insert into temp_use_stock_table {sqltext}")
+            
+            result=duckdb.sql(f"copy ({sqltext}) TO '{str(output_file)}' (format csv, header true, delimiter ';')")
+          
         duckdb.sql("drop table if exists temp_invent_trans_table")
     duckdb.sql("DROP TABLE IF EXISTS temp_start_stock_table")
-
+    #высчитываем скорость отработки функции 
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Общее время : {execution_time:.2f} секунд.")
-    # 2. Оптимально: С помощью DuckDB один раз агрегируем тяжелые 4ГБ файлы движений 
-    # из папки PATH_TRANS во временную легкую таблицу в памяти.
     
-    # 3. Запускаем цикл по дням (от старта к концу):
-        # - Берем файл 'вчера' из папки PATH_STOCK
-        # - Берем изменения за 'сегодня' из нашей легкой таблицы
-        # - Считаем новые остатки
-        # - Сохраняем новый файл 'сегодня' в папку PATH_STOCK
     pass
-
+#выводим необходимые дни месяца 
 def get_days_in_month(year: int, month: int) -> int:
     match month:
         case 4 | 6 | 9 | 11:
@@ -81,7 +85,7 @@ def get_days_in_month(year: int, month: int) -> int:
             raise ValueError("Неверный номер месяца")
         
 
-
+#обратабываем ошибки
 if __name__ == "__main__":
     try:
         main()
